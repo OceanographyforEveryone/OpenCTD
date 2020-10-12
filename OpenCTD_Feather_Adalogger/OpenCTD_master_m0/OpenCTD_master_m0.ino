aff33a4a7b9b8d18df60099ad233d3246726e74c
Kyle W-M <25kylworc@gmail.com>
2020-10-12 09:52:27 -0700

    Now it starts a new data log each time it is powered up
    Big Changes:
    1) Makes a new data log each time it is powered up rather than appending to the old one.
    2) Uses the CSV file format (Comma Separated Values) for easier use in programs like excel and others.
    3) First column contains the Date & Time with a space inbetween (I found Excel & Python parsed this date time format better)
         ^ This is easy to switch back (see the note in the get_date_time_string function)
    
    Improvements:
    1) Data log files are given the correct date created and date modified file metadata (shows up in file explorer / finder)
    2) Temp sensors will now show NAN (Not a number) in the log if one somehow becomes disconnected or stops working.
    3) Made the printing of the date & time much neater using the 'sprinf' c function.

--- OpenCTD_Feather_Adalogger/OpenCTD_master_m0/OpenCTD_master_m0.ino
+++ OpenCTD_Feather_Adalogger/OpenCTD_master_m0/OpenCTD_master_m0.ino
@@ -20,11 +20,12 @@
 RTC_DS3231 rtc; //define real-time clock
 
 const int chipSelect = 4; //sets chip select pin for SD card reader
+char datalogFileName[12];
 
 /* This integer specifies how high accuracy you want your pressure sensor to be (oversampling resolution).
- * Ok values: 256, 512, 1024, 2048, or 4096 (Higher = more accuracy but slower sampling frequency**)
+   Ok values: 256, 512, 1024, 2048, or 4096 (Higher = more accuracy but slower sampling frequency**)
  * ** There is no reason not to use the highest accuracy. This is because the datalogging rate is set by the
- * sampling/response frequency of the ec sensor [default = 1 sec] (this is to avoid the case where both sensors send data at the same time). */
+   sampling/response frequency of the ec sensor [default = 1 sec] (this is to avoid the case where both sensors send data at the same time). */
 #define PRESSURE_SENSOR_RESOLUTION 4096
 MS_5803 sensor = MS_5803(PRESSURE_SENSOR_RESOLUTION); // Define pressure sensor.
 
@@ -36,9 +37,9 @@ SoftwareSerial ecSerial(12, 13); // Define the SoftwareSerial port for conductiv
 double pressure_abs; //define absolute pressure variable
 
 /* This integer specifies how high resolution you want your temperature sensors to be.
- * Ok values: 9,10,11,12 (Higher = more accuracy but slower sampling frequency**)
+   Ok values: 9,10,11,12 (Higher = more accuracy but slower sampling frequency**)
  * ** There is no reason not to use the highest accuracy. This is because the datalogging rate is set by the
- * sampling/response frequency of the ec sensor [default = 1 second] (this is to avoid the case where both sensors send data at the same time). */
+   sampling/response frequency of the ec sensor [default = 1 second] (this is to avoid the case where both sensors send data at the same time). */
 #define TEMP_SENSOR_RESOLUTION 12
 
 //Declare global temperature variables.
@@ -57,7 +58,7 @@ char *EC; // Character pointer for string parsing.
 byte received_from_sensor = 0; // How many characters have been received.
 byte string_received = 0; // Whether it received a string from the EC circuit.
 
-#define EC_SAMPLING_FREQUENCY 1 // Set the sampling frequency in seconds (no decimals) of the conductivity probe (which by extension sets the overall frequency of logging to the sd).
+#define EC_SAMPLING_FREQUENCY 1 // Set the requested sampling frequency of the conductivity probe in seconds (NO Decimals) (this by extension sets the overall frequency of logging).
 
 void setup () {
 
@@ -74,9 +75,13 @@ void setup () {
   while (!SD.begin(chipSelect)) {
 
     Serial.println("Card failed, or not present");
-    return;
+    delay(1000);
 
   }
+
+  // This funny function allows the sd-library to set the correct file created & modified dates for all sd card files.
+  // (See the SDCardDateTimeCallback function defined at the end of this file)
+  SdFile::dateTimeCallback(SDCardDateTimeCallback);
 
   Serial.println("card initialized.");
 
@@ -89,13 +94,16 @@ void setup () {
 
   }
 
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  get_numbered_filename(datalogFileName, "LOG", "CSV");
+
+  Serial.print("Writing to datalog: ");
+  Serial.println(datalogFileName);
+
+  File dataFile = SD.open(datalogFileName, FILE_WRITE);
 
   if (dataFile) {
 
-    dataFile.println("");
-    dataFile.println("=== New Cast ===");
-    dataFile.println("");
+    dataFile.println("Date Time,Pressure,Temp A,Temp B,Temp C,Conductivity");
     dataFile.close();
 
   }
@@ -109,17 +117,19 @@ void setup () {
 
   }
 
-  delay(250);       // Wait a quarter second to continue.
+  delay(250);   // Wait a quarter second to continue.
 
   //Initialize sensors
+  Serial.println("-- Pressure Sensor Info: --");
   sensor.initializeMS_5803(); // Initialize pressure sensor
-
+  Serial.println("---------------------------");
+  
   sensors.begin();  // Intialize the temperature sensors.
   sensors.setResolution(TEMP_SENSOR_RESOLUTION);  // Set the resolution (accuracy) of the temperature sensors.
   sensors.requestTemperatures(); // on the first pass request all temperatures in a blocking way to start the variables with true data.
-  tempA = sensors.getTempCByIndex(0);
-  tempB = sensors.getTempCByIndex(1);
-  tempC = sensors.getTempCByIndex(2);
+  tempA = get_temp_c_by_index(0);
+  tempB = get_temp_c_by_index(1);
+  tempC = get_temp_c_by_index(2);
   sensors.setWaitForConversion(false);  // Now tell the Dallas Temperature library to not block this script while it's waiting for the temperature mesurement to happen
 
   ecSerial.begin(9600); // Set baud rate for conductivity circuit.
@@ -134,17 +144,17 @@ void setup () {
   } while (EC_data[1] != 'I'); // Keep looping until the ecSerial has sent the board info string (also indicating it has booted up, I think...)
 
   Serial.print("EC Board Info (Format: ?I,[board type],[Firmware Version]) -> "); Serial.println(EC_data);
-  
+
   delay(10);
   ecSerial.write('C');  // Tell electrical conductivity board to continously ("C") transmit mesurements ...
   ecSerial.write(',');  //
   ecSerial.print(EC_SAMPLING_FREQUENCY);    // ... every x seconds (here x is the EC_SAMPLING_FREQUENCY variable)
-  ecSerial.write('\r'); // Finish the command with the carrage return character. 
-  
+  ecSerial.write('\r'); // Finish the command with the carrage return character.
+
   received_from_sensor = ecSerial.readBytesUntil('\r', EC_data, 10); // keep reading the reply until the return character is recived (or it gets to be 10 characters long, which shouldn't happen)
   EC_data[received_from_sensor] = 0; // Null terminate the data by setting the value after the final character to 0.
   Serial.print("EC Frequency Set Sucessfully? -> "); Serial.println(EC_data);
-
+  Serial.println("--- Starting Datalogging ---");
 }
 
 void loop () {
@@ -163,20 +173,20 @@ void loop () {
 
     // Read the temperature sensors.
     if (millis() - tempADelayStartTime > requiredMesurementDelay) { // wait for conversion to happen before attempting to read temp probe A's value;
-      tempA = sensors.getTempCByIndex(0);
+      tempA = get_temp_c_by_index(0);
       sensors.requestTemperaturesByIndex(0);  // request temp sensor A start mesuring so it can be read on the following loop (if enough time elapses).
       tempADelayStartTime = millis();  // mark when we made the request to make sure we wait long enough before reading it.
     }
 
     if (millis() - tempBDelayStartTime > requiredMesurementDelay) { // wait for conversion to happen before attempting to read temp probe B's value;
-      tempB = sensors.getTempCByIndex(1);
+      tempB = get_temp_c_by_index(1);
       sensors.requestTemperaturesByIndex(1);  // request temp sensor B start mesuring so it can be read on the following loop (if enough time elapses).
       tempBDelayStartTime = millis();  // mark when we made the request to make sure we wait long enough before reading it.
     }
 
 
     if (millis() - tempCDelayStartTime > requiredMesurementDelay) { // wait for conversion to happen before attempting to read temp probe C's value;
-      tempC = sensors.getTempCByIndex(2);
+      tempC = get_temp_c_by_index(2);
       sensors.requestTemperaturesByIndex(2);  // request temp sensor C start mesuring so it can be read on the following loop (if enough time elapses).
       tempCDelayStartTime = millis(); // mark when we made the request to make sure we wait long enough before reading it.
     }
@@ -185,185 +195,36 @@ void loop () {
     pressure_abs = sensor.pressure();
 
     DateTime now = rtc.now(); //check RTC
+    char dateTimeString[40];
+    get_date_time_string(dateTimeString, now);
 
-    //output readings to serial monitor. A whole mess of if/else statements to keep numbers formatted correctly
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-
-    if (now.month() < 10) {
-
-      Serial.print(0);
-      Serial.print(now.month(), DEC);
-
-    }
-
-    else {
-
-      Serial.print(now.month(), DEC);
-
-    }
-
-    Serial.print('/');
-
-    if (now.day() < 10) {
-
-      Serial.print(0);
-      Serial.print(now.day(), DEC);
-
-    }
-
-    else {
-
-      Serial.print(now.day(), DEC);
-
-    }
-
-    Serial.print("  ");
-
-    if (now.hour() < 10) {
-
-      Serial.print(0);
-      Serial.print(now.hour(), DEC);
-
-    }
-
-    else {
-
-      Serial.print(now.hour(), DEC);
-
-    }
-
-    Serial.print(':');
-
-    if (now.minute() < 10) {
-
-      Serial.print(0);
-      Serial.print(now.minute(), DEC);
-
-    }
-
-    else {
-
-      Serial.print(now.minute(), DEC);
-
-    }
-
-    Serial.print(':');
-
-    if (now.second() < 10) {
-
-      Serial.print(0);
-      Serial.print(now.second(), DEC);
-
-    }
-
-    else {
-
-      Serial.print(now.second(), DEC);
-
-    }
-
-    Serial.print("  ");
+    //output readings to serial
+    Serial.print(dateTimeString);
+    Serial.print(",");
     Serial.print(pressure_abs);
-    Serial.print("  ");
+    Serial.print(",");
     Serial.print(tempA);
-    Serial.print("  ");
+    Serial.print(",");
     Serial.print(tempB);
-    Serial.print("  ");
+    Serial.print(",");
     Serial.print(tempC);
-    Serial.print("  ");
+    Serial.print(",");
     Serial.println(EC);
 
-    //output readings to data file. A whole mess of if/else statements to keep numbers formatted correctly
-    File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
+    //output readings to data file.
+    File dataFile = SD.open(datalogFileName, FILE_WRITE);
     if (dataFile) {
 
-      dataFile.print(now.year(), DEC);
-      dataFile.print('/');
-
-      if (now.month() < 10) {
-
-        dataFile.print(0);
-        dataFile.print(now.month(), DEC);
-
-      }
-
-      else {
-
-        dataFile.print(now.month(), DEC);
-
-      }
-
-      dataFile.print('/');
-
-      if (now.day() < 10) {
-
-        dataFile.print(0);
-        dataFile.print(now.day(), DEC);
-
-      }
-
-      else {
-
-        dataFile.print(now.day(), DEC);
-
-      }
-
-      dataFile.print("  ");
-
-      if (now.hour() < 10) {
-
-        dataFile.print(0);
-        dataFile.print(now.hour(), DEC);
-
-      }
-
-      else {
-
-        dataFile.print(now.hour(), DEC);
-
-      }
-
-      dataFile.print(':');
-
-      if (now.minute() < 10) {
-
-        dataFile.print(0);
-        dataFile.print(now.minute(), DEC);
-
-      }
-
-      else {
-
-        dataFile.print(now.minute(), DEC);
-
-      }
-
-      dataFile.print(':');
-
-      if (now.second() < 10) {
-
-        dataFile.print(0);
-        dataFile.print(now.second(), DEC);
-
-      }
-
-      else {
-
-        dataFile.print(now.second(), DEC);
-
-      }
-
-      dataFile.print("  ");
+      dataFile.print(dateTimeString);
+      dataFile.print(",");
       dataFile.print(pressure_abs);
-      dataFile.print("  ");
+      dataFile.print(",");
       dataFile.print(tempA);
-      dataFile.print("  ");
+      dataFile.print(",");
       dataFile.print(tempB);
-      dataFile.print("  ");
+      dataFile.print(",");
       dataFile.print(tempC);
-      dataFile.print("  ");
+      dataFile.print(",");
       dataFile.println(EC);
       dataFile.close();
 
@@ -373,6 +234,51 @@ void loop () {
 
   // Tip: For a slower overall logging frequency, set the EC_SAMPLING_FREQUENCY variable rather than adding a delay (this will avoid the possibility of garbled ec sensor readings)
 
+}
+
+void get_numbered_filename(char* outStr, char* filePrefix, char* fileExtension) {
+
+  // Make base filename
+  sprintf(outStr, "%s000.%s", filePrefix, fileExtension);
+  int namelength = strlen(outStr);
+  if (namelength > 12) Serial.println("Error: filename too long. Shorten your filename to < 5 characters (12 chars total w number & file extension) !");
+
+  // Keep incrementing the number part of filename until we reach an unused filename
+  int i = 1;
+  while (SD.exists(outStr)) {  // keep looping if filename already exists on card. [If the filename doesn't exist, the loop exits, so we found our first unused filename!]
+
+    int hundreds = i / 100;
+    outStr[namelength - 7] = '0' + hundreds;
+    outStr[namelength - 6] = '0' + (i / 10) - (hundreds * 10);
+    outStr[namelength - 5] = '0' + i % 10;
+    i++;
+
+  }
+
+}
+
+void get_date_time_string(char* outStr, DateTime date) {
+  // outputs the date as a date time string,
+  sprintf(outStr, "%02d/%02d/%02d %02d:%02d:%02d", date.month(), date.day(), date.year(), date.hour(), date.minute(), date.second());
+  // Note: If you would like the date & time to be seperate columns chabge the space in the formatting string to a comma - this works because the file type is CSV (Comma Seperated Values)
+}
+
+void SDCardDateTimeCallback(uint16_t* date, uint16_t* time) // This funny function allows the sd-library to set the correct file created & modified dates for all sd card files (As would show up in the file explorer on your computer)
+{
+  DateTime now = rtc.now();
+  *date = FAT_DATE(now.year(), now.month(), now.day());
+  *time = FAT_TIME(now.hour(), now.minute(), now.second());
+}
+
+float get_temp_c_by_index(int sensor_index) {
+  
+  float value = sensors.getTempCByIndex(sensor_index);
+  if (value == DEVICE_DISCONNECTED_C) {
+    return NAN; // Return Not a Number (NAN) to indicate temperature probe has error or is disconnected.
+  } else {
+    return value; // otherwise return the measured value.
+  }
+  
 }
 
 void parse_data() { // Parses data from the EC Circuit.
